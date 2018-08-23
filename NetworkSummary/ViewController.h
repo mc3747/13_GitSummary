35492d6cccce0e19a2ebc9e05a50166603d6ffc4
mc3747 <mc3747@126.com>
2018-08-23 17:33:35 +0800

    180823-文件初始化

---
+++ NetworkSummary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkSummary
+//
+//  Created by gjfax on 2018/8/23.
+//  Copyright © 2018年 macheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform:ios,'8.0'
+
+target 'NetworkSummary' do
+pod 'AFNetworking'
+
+end
